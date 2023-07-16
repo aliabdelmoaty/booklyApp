03fa81b7a7f10e8351f3ddf5e69152d38c26aeb2
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2023-07-17 02:39:29 +0300

    add title to deatials book

--- lib/Features/home/presentation/view_model/widgets/book_details/book_details_view_body.dart
+++ lib/Features/home/presentation/view_model/widgets/book_details/book_details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:tharwatflutter/core/constants/styles.dart';
+import '../home_view/custom_book_item.dart';
 import 'custom_book_details_appBar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -7,14 +8,31 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal:25.0),
+    var width = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
       child: Column(
         children: [
-          CustomBookDetailsAppBar(),
+          const CustomBookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .22),
+            child:  const CustomBookImage(),
+          ),
+          const SizedBox(height: 43,),
+          Text('The Jungle Book',style: Styles.textStyle30,),
+          const SizedBox(height: 6,),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
-

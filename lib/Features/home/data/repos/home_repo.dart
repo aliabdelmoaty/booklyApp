2459a37490f098be972fa96dab3d67deb83cc861
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2023-07-17 14:38:24 +0300

    implement fetch newset books

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -4,6 +4,6 @@ import 'package:tharwatflutter/Features/home/data/models/book_model/book_model.d
 import '../../../../core/errors/fails.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failure,List<BookModel>>> fetchBestSellerBook();
-  Future<Either<Failure,List<BookModel>>> fetchFeaturedBook();
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBook();
 }
--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -1,14 +1,28 @@
 import 'package:dartz/dartz.dart';
-import 'package:dio/dio.dart';
 import 'package:tharwatflutter/Features/home/data/models/book_model/book_model.dart';
 import 'package:tharwatflutter/Features/home/data/repos/home_repo.dart';
+import 'package:tharwatflutter/core/constants/api_service.dart';
 import 'package:tharwatflutter/core/errors/fails.dart';
 
-class HomeRepoImpl implements HomeRepo{
+class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBook() {
-    // TODO: implement fetchFeaturedBook
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks() async {
+    var data;
+    data = await apiService
+        .get(
+            endpoint:
+                'volumes?Filtering=free-ebooks&Sorting=newest&subject=programming&q=subject:programming')
+        .then((value) {
+      List<BookModel> books = [];
+      for (var item in data['item']) {
+        books.add(item);
+      }
+      return right(books);
+    });
+    return left(ServerFailure());
   }
 
   @override

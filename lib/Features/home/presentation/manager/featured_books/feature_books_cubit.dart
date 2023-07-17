1add04e4e6f5596e74184aa0c598c125395ac0cf
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2023-07-18 02:31:24 +0300

    create feature books method

--- lib/Features/home/presentation/manager/featured_books/feature_books_cubit.dart
+++ lib/Features/home/presentation/manager/featured_books/feature_books_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:tharwatflutter/Features/home/data/models/book_model/book_model.dart';
+import 'package:tharwatflutter/Features/home/data/repos/home_repo.dart';
 
 part 'feature_books_state.dart';
 
 class FeatureBooksCubit extends Cubit<FeatureBooksState> {
-  FeatureBooksCubit() : super(FeatureBooksInitial());
+  FeatureBooksCubit(this.homeRepo) : super(FeatureBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchFeatureBooks() async {
+    emit(FeatureBooksLoading());
+    var result = await homeRepo.fetchFeaturedBook();
+    result.fold((failure) {
+      emit(FeatureBooksFailure(failure.errorMessage));
+    }, (books) {
+      emit(FeatureBooksSuccess(books));
+    });
+  }
 }

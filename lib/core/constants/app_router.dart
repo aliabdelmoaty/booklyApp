56fb3c6d38e53c895347754c86c879c78d1ece2f
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2023-07-17 07:32:39 +0300

    create search view

--- lib/core/constants/app_router.dart
+++ lib/core/constants/app_router.dart
@@ -1,5 +1,6 @@
 
 import 'package:tharwatflutter/Features/home/presentation/view_model/home_view.dart';
+import 'package:tharwatflutter/Features/search/presentation/views/search_view.dart';
 
 import '../../Features/Splash/presentation/view_models/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
@@ -9,6 +10,8 @@ import '../../Features/home/presentation/view_model/book_details.dart';
 abstract class AppRouter{
   static const kHomeView ='/homeView';
   static const kBookDetailsView ='/bookDetailsView';
+  static const kSearchView = '/searchView';
+
   static  final router = GoRouter(
     routes: [
       GoRoute(
@@ -21,7 +24,11 @@ abstract class AppRouter{
       ),
       GoRoute(
         path: kBookDetailsView,
-        builder: (context, state) =>  const BookDetailsView(),
+        builder: (context, state) => const BookDetailsView(),
+      ),
+      GoRoute(
+        path: kSearchView,
+        builder: (context, state) =>  const SearchView(),
       ),
     ],
   );

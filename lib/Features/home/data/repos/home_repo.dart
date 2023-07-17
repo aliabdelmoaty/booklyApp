473db3343557ec515fd88dac185e195c0fd268b3
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2023-07-17 12:24:04 +0300

    create home repo method and failure file

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,5 +1,9 @@
-abstract class HomeRepo {
-  fetchBestSellerBook();
-  fetchFeaturedBook();
+import 'package:dartz/dartz.dart';
+import 'package:tharwatflutter/Features/home/data/models/book_model/book_model.dart';
 
+import '../../../../core/errors/fails.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure,List<BookModel>>> fetchBestSellerBook();
+  Future<Either<Failure,List<BookModel>>> fetchFeaturedBook();
 }
